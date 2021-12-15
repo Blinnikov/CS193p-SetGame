dab437f03b18dc0892212d609128a01a999c6dd9
Igor Blinnikov <igor.blinnikov@gmail.com>
2021-12-16 01:30:32 +0300

    feat(set): add empty project

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Igor Blinnikov on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
